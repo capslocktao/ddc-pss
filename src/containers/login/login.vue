52f6cca71fcf29d50516f7415dd8fc6412b0175b
capslocktao <haitaocapslock@outlook.com>
2018-03-01 14:45:58 +0800

    配置项目

---
+++ src/containers/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+      <Button type="primary">Primary</Button>
+    </div>
+</template>
+
+<script>
+  import { Button } from 'iview'
+    export default {
+        name: "login"
+    }
+</script>
+
+<style lang="less">
+  @import './login.less';
+</style>
